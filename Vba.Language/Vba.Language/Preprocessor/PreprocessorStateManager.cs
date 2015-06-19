622238cc488957d7e427f12572311f37af74f442
Ross Knudsen <ross@gravitycomputing.co.nz>
2015-06-20 11:03:33 +1200

    Renamed StateManager to PreprocessorStateManager Added some inplementation in PreprocessorStateManager.

--- Vba.Language/Vba.Language/Preprocessor/PreprocessorStateManager.cs
+++ Vba.Language/Vba.Language/Preprocessor/PreprocessorStateManager.cs
@@ -1,5 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+
+using Antlr4.Runtime;
+
+using Vba.Grammars;
 
 namespace Vba.Language.Preprocessor
 {
@@ -7,17 +12,28 @@ namespace Vba.Language.Preprocessor
     {
         private Stack<PreprocessorState> stateStack;
         private IDictionary<string, object> compilerConstants;
-        // member Antlr lexer/parser...
+        PreprocessorLexer lexer;
+        PreprocessorParser parser; 
 
         public PreprocessorStateManager()
         {
             stateStack = new Stack<PreprocessorState>();
             compilerConstants = new Dictionary<string, object>();
+            lexer = new PreprocessorLexer(new AntlrInputStream(""));
+            var tokens = new CommonTokenStream(lexer);
+            parser = new PreprocessorParser(tokens);
         }
 
         internal void ExecutePreprocessorStatement(string line)
         {
-            // use Antlr parser/lexer to process line.
+            if (line.Contains("\n") || line.Contains("\r"))
+            {
+                throw new ArgumentException("Parameter line cannot contain line break characters.");
+            }
+
+            var input = new AntlrInputStream(line);
+            lexer.SetInputStream(input);
+            var tree = parser.preprocessorStatement();
 
             // TODO how to handle syntax errors.
 
