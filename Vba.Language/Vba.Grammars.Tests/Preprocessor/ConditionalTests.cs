cf1ab71bf9ef99db68cbb2b3bc223879d293fccd
Ross Knudsen <ross@gravitycomputing.co.nz>
2015-06-26 22:12:12 +1200

    Added some conditional tests.

---
+++ Vba.Language/Vba.Grammars.Tests/Preprocessor/ConditionalTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Vba.Grammars.Tests.Preprocessor
+{
+    public class ConditionalTests
+    {
+        [Theory]
+        [InlineData("#If True Then", "True")]
+        [InlineData("#If False Then", "False")]
+        [InlineData("#If abc = True Then", "abc = True")]
+        [InlineData("#If abc = False Then", "abc = False")]
+        public void CanParseValidIfStatement(string source, string expression)
+        {
+            var parser = PreprocessorHelper.BuildPreprocessorParser(source);
+
+            var result = parser.ifStatement();
+
+            Assert.Equal(expression, result.expression().GetText());
+            Assert.Null(result.exception);
+        }
+    }
+}
