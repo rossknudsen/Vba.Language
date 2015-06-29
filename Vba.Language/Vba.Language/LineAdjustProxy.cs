5b2e3c843bc29afaaf856db569c4b375924e4106
Ross Knudsen <ross@gravitycomputing.co.nz>
2015-06-30 09:59:20 +1200

    Added LineAdjustProxy class which sits between the VbaLexer and VbaParser and alters the Line to match the original line numbers in the source file.

---
+++ Vba.Language/Vba.Language/LineAdjustProxy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Antlr4.Runtime;
+
+namespace Vba.Language
+{
+    /// <summary>
+    /// This class is a proxy for a true ITokenSource and changes the behaviour of the Line method.  It is used
+    /// to adjust the line numbers which the VBA lexer sees to match the true line number that the Preprocessor
+    /// sees in the source.
+    /// </summary>
+    internal class LineAdjustProxy : ITokenSource
+    {
+        private readonly ITokenSource tokens;
+        private readonly Func<int> getLineFunc;
+
+        public LineAdjustProxy(ITokenSource tokens, Func<int> getLineFunc)
+        {
+            this.tokens = tokens;
+            this.getLineFunc = getLineFunc;
+        }
+
+        public IToken NextToken()
+        {
+            return tokens.NextToken();
+        }
+
+        public int Line
+        {
+            get { return getLineFunc(); }
+        }
+
+        public int Column
+        {
+            get { return tokens.Column; }
+        }
+
+        public ICharStream InputStream
+        {
+            get { return tokens.InputStream; }
+        }
+
+        public string SourceName
+        {
+            get { return tokens.SourceName; }
+        }
+
+        public ITokenFactory TokenFactory
+        {
+            get { return tokens.TokenFactory; }
+            set { tokens.TokenFactory = value; }
+        }
+    }
+}
