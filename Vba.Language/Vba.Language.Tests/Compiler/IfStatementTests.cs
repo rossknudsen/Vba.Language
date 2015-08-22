0c67c5d7d79484d81d44c30b6b8f14ed9e23c88f
Ross Knudsen <rossknudsen@gmail.com>
2015-08-22 22:08:58 +1200

    Added If Statement tests.

---
+++ Vba.Language/Vba.Language.Tests/Compiler/IfStatementTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Vba.Language.Tests.Compiler
+{
+    public class IfStatementTests
+    {
+        [Fact]
+        public void CanParseSimpleMultilineIfStatement()
+        {
+            var source = "If True Then\r\nEnd If\r\n";
+            var regex = @"(\(ifStatement.*True.*\))";
+
+            ParserHelper.CanParseSourceRegex(source, regex, p => p.statementBlock());
+        }
+    }
+}
